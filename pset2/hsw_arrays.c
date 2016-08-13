f0bc1bc5e222fe266bad5f4f6a65d9ef1360a9ac
bmind12 <manchester1291@gmail.com>
2016-08-13 13:10:21 +0200

    bubble sorting

--- pset2/hsw_arrays.c
+++ pset2/hsw_arrays.c
@@ -8,7 +8,7 @@ int rand_seed=10;
 int rand()
 {
   rand_seed = rand_seed * 1103515245 +12345;
-  return (unsigned int)(rand_seed / 65536) % 32768
+  return (unsigned int)(rand_seed / 65536) % 32768;
 }
 
 int main()
@@ -17,9 +17,22 @@ int main()
 
   for (i=0; i < MAX; i++)
   {
-    a=rand()
+    a=rand();
     printf("%d\n", a);
   }
+
+  for (x=0; x < MAX-1; x++)
+    for (y=0; y < MAX-x-1; y++)
+      if (a > a[y+1])
+      {
+        t = a;
+        a = a[y+1];
+        a[y+1] = t;
+      }
+
+  printf("-------------------\n");
+  for (i=0; i < MAX; i++)
+  printf("%d\n", a);
 
   return 0;
 }

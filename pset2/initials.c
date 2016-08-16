031e06d86a2df3eac8d2e540fb5ad84c5d3ecccb
bmind12 <manchester1291@gmail.com>
2016-08-16 20:45:53 +0200

    initials solved

--- pset2/initials.c
+++ pset2/initials.c
@@ -2,33 +2,35 @@
 #include <cs50.h>
 #include <string.h>
 
-string capital(string abb);
+string capital(string str);
+int abblen;
 
 int main()
 {
   string name = GetString();
-  int namelen = strlen(name);
-  char abb[namelen];
+  char abb[strlen(name)];
   abb[0] = name[0];
   int a = 1;
 
-  for (int i = 1; i < namelen; i++) {
+  for (int i = 1, sl = strlen(name); i < sl; i++) {
     if (name[i-1] == ' ')
     {
        abb[a] = name[i];
        a++;
     }
   }
+  abblen = strlen(abb);
   printf("%s\n", capital(abb));
 }
 
 string capital(string str)
 {
-  int len = strlen(str);
 
-  for (int i = 0; i < len; i++)
+  for (int i = 0; i < abblen; i++)
   {
-    if ((int) str[i] < 97)
-      str[i] = (char) (int) str[i] + 32;
+    if (str[i] >= 'a' && str[i] <= 'z')
+      str[i] = str[i] - ('a' - 'A');
   }
+
+  return str;
 }

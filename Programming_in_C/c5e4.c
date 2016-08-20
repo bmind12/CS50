b4f12bc0015cc621a86e3a6be7bcbc7cb792630c
bmind12 <manchester1291@gmail.com>
2016-08-20 11:51:19 +0200

    c5e4 done

---
+++ Programming_in_C/c5e4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float s, n;
+  char o;
+
+  do {
+
+    scanf("%f %c", &n, &o);
+
+    switch (o) {
+      case '+':
+        s = s + n;
+        break;
+      case '-':
+        s = s - n;
+        break;
+      case '*':
+        s = s * n;
+        break;
+      case '/':
+        if (n == 0) {
+          printf("Division by 0 is not possible\n");
+          break;
+        }
+        s = s / n;
+        break;
+      case 'S':
+        s = n;
+        break;
+      default:
+        printf("Operator is unknown\n");
+    }
+
+    s > 0 ? printf("= %f\n", s) : printf("%f\n", s);
+
+  } while (o != 'E');
+
+    printf("End of calculations\n");
+
+  return 0;
+}
